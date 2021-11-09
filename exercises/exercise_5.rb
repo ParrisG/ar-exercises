318bf6e371361c64cb0af20334b7914c39ce58f7
Parris Greaves <parris.greaves@gmail.com>
2021-11-08 21:23:14 -0500

    exercise 5 completed

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts total_revenue
+puts total_revenue / Store.all.count
+puts Store.where('annual_revenue > 1000000').size
